d8dc5080b094c4988b2eefa2be408f57fd971196
Alvaro Rabadan <alvaro.rabadan@gmail.com>
2021-04-10 05:49:19 +0200

    Fixed insert many items and build history

--- update_history.py
+++ update_history.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
+from db_manager import Postgres
+from utils import get_timestamp, from_pd_to_jsons
 import euribor
-from db_manager import Postgre
+
 
 TABLE_NAME = 'orchard.euribor'
 
 if __name__ == '__main__':
     ec = euribor.EuriborCrawl()
-    db = Postgre()
+    db = Postgres()
     # Build history 
     items = ec.build_history()
     # We have to add a new column: time stamp insertion 
-    now = datetime.now()
+    now = get_timestamp()
+    items['date_insertion'] = now
 
     # Transform to json 
-    items_json = euribor.from_pd_to_jsons(items)
-    # Get keys example and give them the format we will use 
-    # eur_date, eur_1w, eur_1m ...
+    items_json = from_pd_to_jsons(items)
+    # Get keys example
     first_item = items_json[0]
-    keys = ['_'.join(['eur',(i)]) for i in first_item.keys()]
+    keys = [key for key in first_item.keys()]
     # Get values 
     values = [[value for value in entry.values()] for entry in items_json]
     # Insert them into the DB 
