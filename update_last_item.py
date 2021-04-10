90bddecee1c6bc9ca37f64803bf88fa1fb358917
Alvaro Rabadan <alvaro.rabadan@gmail.com>
2021-04-10 05:53:50 +0200

    TODO: Check if the last item available has been already include

--- update_last_item.py
+++ update_last_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 from euribor import EuriborCrawl
-from db_manager import Postgre
+from db_manager import Postgres
 
 TABLE_NAME = 'orchard.euribor'
 
 def update_last_item():
+    # TODO: Check if last working day has been already included into the system
     ec = EuriborCrawl()
-    db = Postgre()
+    db = Postgres()
     last_item = ec.get_last_euribor_rate()
     db.insert_dict(TABLE_NAME, last_item)
     db.close()
