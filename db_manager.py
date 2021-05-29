21d6f05c924874a3b37957811cedabb0db9773b6
Alvaro Rabadan <alvaro.rabadan@gmail.com>
2021-05-29 11:59:48 +0200

    :sparkles: History Updater: including functionalities for BigQuery and PostgreSQL, added setup.ini config

--- db_manager.py
+++ db_manager.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 import psycopg2
 from psycopg2.extras import execute_values
-from config import get_db_config 
+from config import get_db_config
 
+# Big Query
+# https://cloud.google.com/bigquery/docs/authentication/service-account-file
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 class Postgres:
     def __init__(self):
@@ -42,3 +46,55 @@ class Postgres:
             print(error)
         finally:
             self.conn.commit()
+
+    
+class BigQuery:
+    def __init__(self):
+        
+        try:
+            self.bigquery = bigquery
+            self.params = get_db_config(section='gcp')
+            credentials = service_account.Credentials.from_service_account_file(
+                self.params['credentials_file_path'], 
+                scopes=["https://www.googleapis.com/auth/cloud-platform"],
+            )
+            self.client = bigquery.Client(
+                credentials=credentials, 
+                project=credentials.project_id,
+            )         
+
+        except Exception as error:
+            raise(error)
+    
+    def insert_many_dict(self, table_id, list_of_dicts):
+        '''
+        STREAM DATA NOT FREE
+        https://cloud.google.com/bigquery/docs/samples/bigquery-load-table-gcs-json
+        '''
+        errors = self.client.insert_rows_json(table_id, list_of_dicts) 
+        if errors == []:
+            print("New rows have been added.")
+        else:
+            print("Encountered errors while inserting rows: {}".format(errors))
+
+    def insert_dataframe(self, table_id, df, dict_schema):
+        '''
+        https://cloud.google.com/bigquery/docs/samples/bigquery-load-table-dataframe
+        '''
+        job_config = bigquery.LoadJobConfig(
+            schema=dict_schema,
+            write_disposition="WRITE_TRUNCATE"
+        )
+
+        job=self.client.load_table_from_dataframe(
+            df,
+            table_id,
+            job_config
+        )
+        job.result()
+        table = self.client.get_table(table_id)  # Make an API request.
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), table_id
+            )
+        )
---
+++ update_history_bigquery.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+from google.cloud import bigquery
+from google.cloud.bigquery.enums import SqlTypeNames
+from google.cloud.bigquery.schema import SchemaField
+from db_manager import BigQuery
+from utils import get_timestamp, from_pd_to_jsons
+import euribor
+import pandas as pd 
+
+
+TABLE_NAME = 'orchard-euribor.orchard.euribor'
+
+if __name__ == '__main__':
+    ec = euribor.EuriborCrawl()
+    db = BigQuery()
+    # Build history 
+    items = ec.build_history()
+    # We have to add a new column: time stamp insertion 
+    now = get_timestamp()
+    items['date_insertion'] = now
+    items['date_insertion'] = pd.to_datetime(items['date_insertion'])
+    schema=[
+        db.bigquery.SchemaField("eur_date", db.bigquery.enums.SqlTypeNames.TIMESTAMP),
+        db.bigquery.SchemaField("eur_1w", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("eur_1m", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("eur_3m", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("eur_6m", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("eur_12m", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("eur_year", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("eur_month", db.bigquery.enums.SqlTypeNames.NUMERIC),
+        db.bigquery.SchemaField("date_insertion", db.bigquery.enums.SqlTypeNames.TIMESTAMP),
+    ]
+    db.insert_dataframe(TABLE_NAME,items,schema)
+    
